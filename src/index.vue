30afcb3a95ac85e589c52f72ca7de903de01dc75
xjx0106 <xujingxin@dataexa.com>
2020-11-26 11:46:47 +0800

    添加词性关系画布div

---
+++ src/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="main" id="canvasContainer">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WordRelationship',
+  components: {},
+  data() {
+    return {
+
+    }
+  },
+  created: {
+
+  },
+  mounted() {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+.main {
+  border: 1px solid red;
+  height: 500px;
+  width: 100%;
+}
+
+
+</style>
